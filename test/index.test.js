82cc98e36079f937ac8d1688e7524d7878f820d6
soyuka <soyuka@gmail.com>
2017-06-28 20:08:08 +0200

    fix undefined issue, add some tests, use standardjs lint

--- test/index.test.js
+++ test/index.test.js
@@ -3,9 +3,9 @@
  * Module requirements.
  */
 
-require('should');
+require('should')
 
-var Table = require('../');
+var Table = require('../')
 
 /**
  * Tests.
@@ -13,266 +13,277 @@ var Table = require('../');
 
 module.exports = {
 
-  'test complete table': function (){
+  'test complete table': function () {
     var table = new Table({
-        head: ['Rel', 'Change', 'By', 'When']
-      , style: {
-            'padding-left': 1
-          , 'padding-right': 1
-          , head: []
-          , border: []
-        }
-      , colWidths: [6, 21, 25, 17]
-    });
+      head: ['Rel', 'Change', 'By', 'When'],
+      style: {
+        'padding-left': 1,
+        'padding-right': 1,
+        head: [],
+        border: []
+      },
+      colWidths: [6, 21, 25, 17]
+    })
 
     table.push(
-        ['v0.1', 'Testing something cool', 'rauchg@gmail.com', '7 minutes ago']
-      , ['v0.1', 'Testing something cool', 'rauchg@gmail.com', '8 minutes ago']
-    );
+      ['v0.1', 'Testing something cool', 'rauchg@gmail.com', '7 minutes ago'],
+      ['v0.1', 'Testing something cool', 'rauchg@gmail.com', '8 minutes ago']
+    )
 
     var expected = [
-        '┌──────┬─────────────────────┬─────────────────────────┬─────────────────┐'
-      , '│ Rel  │ Change              │ By                      │ When            │'
-      , '├──────┼─────────────────────┼─────────────────────────┼─────────────────┤'
-      , '│ v0.1 │ Testing something … │ rauchg@gmail.com        │ 7 minutes ago   │'
-      , '├──────┼─────────────────────┼─────────────────────────┼─────────────────┤'
-      , '│ v0.1 │ Testing something … │ rauchg@gmail.com        │ 8 minutes ago   │'
-      , '└──────┴─────────────────────┴─────────────────────────┴─────────────────┘'
-    ];
+      '┌──────┬─────────────────────┬─────────────────────────┬─────────────────┐',
+      '│ Rel  │ Change              │ By                      │ When            │',
+      '├──────┼─────────────────────┼─────────────────────────┼─────────────────┤',
+      '│ v0.1 │ Testing something … │ rauchg@gmail.com        │ 7 minutes ago   │',
+      '├──────┼─────────────────────┼─────────────────────────┼─────────────────┤',
+      '│ v0.1 │ Testing something … │ rauchg@gmail.com        │ 8 minutes ago   │',
+      '└──────┴─────────────────────┴─────────────────────────┴─────────────────┘'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
-    table.render().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
+    table.render().should.eql(expected.join('\n'))
   },
 
-  'test width property': function (){
+  'test width property': function () {
     var table = new Table({
-        head: ['Cool'],
-        style: {
-          head: [],
-          border: []
-        }
-    });
+      head: ['Cool'],
+      style: {
+        head: [],
+        border: []
+      }
+    })
 
-    table.width.should.eql(8);
+    table.width.should.eql(8)
   },
 
-  'test vertical table output': function() {
-    var table = new Table({ style: {'padding-left':0, 'padding-right':0, head:[], border:[]} }); // clear styles to prevent color output
+  'test vertical table output': function () {
+    var table = new Table({ style: {'padding-left': 0, 'padding-right': 0, head: [], border: []} }) // clear styles to prevent color output
 
     table.push(
-        {'v0.1': 'Testing something cool'}
-      , {'v0.1': 'Testing something cool'}
-    );
+      {'v0.1': 'Testing something cool'},
+      {'v0.1': 'Testing something cool'}
+    )
 
     var expected = [
-        '┌────┬──────────────────────┐'
-      , '│v0.1│Testing something cool│'
-      , '├────┼──────────────────────┤'
-      , '│v0.1│Testing something cool│'
-      , '└────┴──────────────────────┘'
-    ];
+      '┌────┬──────────────────────┐',
+      '│v0.1│Testing something cool│',
+      '├────┼──────────────────────┤',
+      '│v0.1│Testing something cool│',
+      '└────┴──────────────────────┘'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   },
 
-  'test cross table output': function() {
-    var table = new Table({ head: ["", "Header 1", "Header 2"], style: {'padding-left':0, 'padding-right':0, head:[], border:[]} }); // clear styles to prevent color output
+  'test cross table output': function () {
+    var table = new Table({ head: ['', 'Header 1', 'Header 2'], style: {'padding-left': 0, 'padding-right': 0, head: [], border: []} }) // clear styles to prevent color output
 
     table.push(
-        {"Header 3": ['v0.1', 'Testing something cool'] }
-      , {"Header 4": ['v0.1', 'Testing something cool'] }
-    );
+      {'Header 3': ['v0.1', 'Testing something cool']},
+      {'Header 4': ['v0.1', 'Testing something cool']}
+    )
 
     var expected = [
-        '┌────────┬────────┬──────────────────────┐'
-      , '│        │Header 1│Header 2              │'
-      , '├────────┼────────┼──────────────────────┤'
-      , '│Header 3│v0.1    │Testing something cool│'
-      , '├────────┼────────┼──────────────────────┤'
-      , '│Header 4│v0.1    │Testing something cool│'
-      , '└────────┴────────┴──────────────────────┘'
-    ];
+      '┌────────┬────────┬──────────────────────┐',
+      '│        │Header 1│Header 2              │',
+      '├────────┼────────┼──────────────────────┤',
+      '│Header 3│v0.1    │Testing something cool│',
+      '├────────┼────────┼──────────────────────┤',
+      '│Header 4│v0.1    │Testing something cool│',
+      '└────────┴────────┴──────────────────────┘'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   },
 
-  'test table colors': function(){
+  'test table colors': function () {
     var table = new Table({
       head: ['Rel', 'By'],
       style: {head: ['red'], border: ['grey']}
-    });
+    })
 
     var off = '\u001b[39m'
-      , red = '\u001b[31m'
-      , orange = '\u001b[38;5;221m'
-      , grey = '\u001b[90m'
+    var red = '\u001b[31m'
+    var orange = '\u001b[38;5;221m'
+    var grey = '\u001b[90m'
 
-      , c256s = orange + 'v0.1' + off;
+      c256s = orange + 'v0.1' + off
 
-    table.push(
-        [c256s, 'rauchg@gmail.com']
-    );
+    table.push([c256s, 'rauchg@gmail.com'])
 
     var expected = [
-        grey + '┌──────┬──────────────────┐' + off
-      , grey + '│' + off + red + ' Rel  ' + off + grey + '│' + off + red + ' By               ' + off + grey + '│' + off
-      , grey + '├──────┼──────────────────┤' + off
-      , grey + '│' + off + ' ' + c256s + ' ' + grey + '│' + off + ' rauchg@gmail.com ' + grey + '│' + off
-      , grey + '└──────┴──────────────────┘' + off
-    ];
+      grey + '┌──────┬──────────────────┐' + off,
+      grey + '│' + off + red + ' Rel  ' + off + grey + '│' + off + red + ' By               ' + off + grey + '│' + off,
+      grey + '├──────┼──────────────────┤' + off,
+      grey + '│' + off + ' ' + c256s + ' ' + grey + '│' + off + ' rauchg@gmail.com ' + grey + '│' + off,
+      grey + '└──────┴──────────────────┘' + off
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   },
 
-  'test custom chars': function (){
+  'test custom chars': function () {
     var table = new Table({
       chars: {
-          'top': '═'
-        , 'top-mid': '╤'
-        , 'top-left': '╔'
-        , 'top-right': '╗'
-        , 'bottom': '═'
-        , 'bottom-mid': '╧'
-        , 'bottom-left': '╚'
-        , 'bottom-right': '╝'
-        , 'left': '║'
-        , 'left-mid': '╟'
-        , 'right': '║'
-        , 'right-mid': '╢'
+        'top': '═',
+        'top-mid': '╤',
+        'top-left': '╔',
+        'top-right': '╗',
+        'bottom': '═',
+        'bottom-mid': '╧',
+        'bottom-left': '╚',
+        'bottom-right': '╝',
+        'left': '║',
+        'left-mid': '╟',
+        'right': '║',
+        'right-mid': '╢'
       },
       style: {
-          head: []
-        , border: []
+        head: [],
+        border: []
       }
-    });
+    })
 
-    table.push(
-        ['foo', 'bar', 'baz']
-      , ['frob', 'bar', 'quuz']
-    );
+    table.push(['foo', 'bar', 'baz'], ['frob', 'bar', 'quuz'])
 
     var expected = [
-        '╔══════╤═════╤══════╗'
-      , '║ foo  │ bar │ baz  ║'
-      , '╟──────┼─────┼──────╢'
-      , '║ frob │ bar │ quuz ║'
-      , '╚══════╧═════╧══════╝'
-    ];
+      '╔══════╤═════╤══════╗',
+      '║ foo  │ bar │ baz  ║',
+      '╟──────┼─────┼──────╢',
+      '║ frob │ bar │ quuz ║',
+      '╚══════╧═════╧══════╝'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   },
 
-  'test compact shortand': function (){
+  'test compact shortand': function () {
     var table = new Table({
       style: {
-          head: []
-        , border: []
-        , compact : true
+        head: [],
+        border: [],
+        compact: true
       }
-    });
+    })
 
-    table.push(
-        ['foo', 'bar', 'baz']
-      , ['frob', 'bar', 'quuz']
-    );
+    table.push(['foo', 'bar', 'baz'], ['frob', 'bar', 'quuz'])
 
     var expected = [
-        '┌──────┬─────┬──────┐'
-      , '│ foo  │ bar │ baz  │'
-      , '│ frob │ bar │ quuz │'
-      , '└──────┴─────┴──────┘'
-    ];
+      '┌──────┬─────┬──────┐',
+      '│ foo  │ bar │ baz  │',
+      '│ frob │ bar │ quuz │',
+      '└──────┴─────┴──────┘'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   },
 
-  'test compact empty mid line': function (){
+  'test compact empty mid line': function () {
     var table = new Table({
       chars: {
-          'mid': ''
-        , 'left-mid': ''
-        , 'mid-mid': ''
-        , 'right-mid': ''
+        'mid': '',
+        'left-mid': '',
+        'mid-mid': '',
+        'right-mid': ''
       },
       style: {
-          head: []
-        , border: []
+        head: [],
+        border: []
       }
-    });
+    })
 
-    table.push(
-        ['foo', 'bar', 'baz']
-      , ['frob', 'bar', 'quuz']
-    );
+    table.push(['foo', 'bar', 'baz'], ['frob', 'bar', 'quuz'])
 
     var expected = [
-        '┌──────┬─────┬──────┐'
-      , '│ foo  │ bar │ baz  │'
-      , '│ frob │ bar │ quuz │'
-      , '└──────┴─────┴──────┘'
-    ];
+      '┌──────┬─────┬──────┐',
+      '│ foo  │ bar │ baz  │',
+      '│ frob │ bar │ quuz │',
+      '└──────┴─────┴──────┘'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   },
 
-  'test decoration lines disabled': function (){
+  'test decoration lines disabled': function () {
     var table = new Table({
       chars: {
-          'top': ''
-        , 'top-mid': ''
-        , 'top-left': ''
-        , 'top-right': ''
-        , 'bottom': ''
-        , 'bottom-mid': ''
-        , 'bottom-left': ''
-        , 'bottom-right': ''
-        , 'left': ''
-        , 'left-mid': ''
-        , 'mid': ''
-        , 'mid-mid': ''
-        , 'right': ''
-        , 'right-mid': ''
-        , 'middle': ' ' // a single space
+        'top': '',
+        'top-mid': '',
+        'top-left': '',
+        'top-right': '',
+        'bottom': '',
+        'bottom-mid': '',
+        'bottom-left': '',
+        'bottom-right': '',
+        'left': '',
+        'left-mid': '',
+        'mid': '',
+        'mid-mid': '',
+        'right': '',
+        'right-mid': '',
+        'middle': ' ' // a single space
       },
       style: {
-          head: []
-        , border: []
-        , 'padding-left': 0
-        , 'padding-right': 0
+        head: [],
+        border: [],
+        'padding-left': 0,
+        'padding-right': 0
       }
-    });
+    })
 
-    table.push(
-        ['foo', 'bar', 'baz']
-      , ['frobnicate', 'bar', 'quuz']
-    );
+    table.push(['foo', 'bar', 'baz'], ['frobnicate', 'bar', 'quuz'])
 
     var expected = [
-        'foo        bar baz '
-      , 'frobnicate bar quuz'
-    ];
+      'foo        bar baz ',
+      'frobnicate bar quuz'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   },
 
-  'test with null/undefined as values or column names': function (){
+  'test with null/undefined as values or column names': function () {
     var table = new Table({
+      head: [null, undefined, 0, ''],
       style: {
-          head: []
-        , border: []
+        head: [],
+        border: []
       }
-    });
+    })
 
     table.push(
-        [null, undefined, 0]
-    );
+        [null, undefined, 0, '']
+    )
 
     var expected = [
-        '┌──┬──┬───┐'
-      , '│  │  │ 0 │'
-      , '└──┴──┴───┘'
-    ];
+      '┌──┬──┬───┬──┐',
+      '│  │  │ 0 │  │',
+      '├──┼──┼───┼──┤',
+      '│  │  │ 0 │  │',
+      '└──┴──┴───┴──┘'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
+  },
+ 'test with toString things': function() {
+
+     var table = new Table({
+      head: ['Some test toString', 'foo'],
+      style: {
+        head: [],
+        border: []
+      }
+    })
+
+    table.push([{}, {toString: function() { return 'foo'}}])
+
+    var expected = [
+      '┌────────────────────┬─────┐',
+      '│ Some test toString │ foo │',
+      '├────────────────────┼─────┤',
+      '│ [object Object]    │ foo │',
+      '└────────────────────┴─────┘'
+    ]
+
+    table.toString().should.eql(expected.join('\n'))
   }
-};
+}
--- test/newlines.test.js
+++ test/newlines.test.js
@@ -2,91 +2,91 @@
  * Module requirements.
  */
 
-require('should');
+require('should')
 
-var Table = require('../');
+var Table = require('../')
 
 /**
  * Tests.
  */
 
 module.exports = {
-  'test table with newlines in headers': function() {
+  'test table with newlines in headers': function () {
     var table = new Table({
-        head: ['Test', "1\n2\n3"]
-      , style: {
-            'padding-left': 1
-          , 'padding-right': 1
-          , head: []
-          , border: []
-        }
-    });
+      head: ['Test', '1\n2\n3'],
+      style: {
+        'padding-left': 1,
+        'padding-right': 1,
+        head: [],
+        border: []
+      }
+    })
 
     var expected = [
-        '┌──────┬───┐'
-      , '│ Test │ 1 │'
-      , '│      │ 2 │'
-      , '│      │ 3 │'
-      , '└──────┴───┘'
-    ];
+      '┌──────┬───┐',
+      '│ Test │ 1 │',
+      '│      │ 2 │',
+      '│      │ 3 │',
+      '└──────┴───┘'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   },
 
-  'test column width is accurately reflected when newlines are present': function() {
-    var table = new Table({ head: ['Test\nWidth'], style: {head:[], border:[]} });
-    table.width.should.eql(9);
+  'test column width is accurately reflected when newlines are present': function () {
+    var table = new Table({ head: ['Test\nWidth'], style: {head: [], border: []} })
+    table.width.should.eql(9)
   },
 
-  'test newlines in body cells': function() {
-    var table = new Table({style: {head:[], border:[]}});
+  'test newlines in body cells': function () {
+    var table = new Table({style: {head: [], border: []}})
 
-    table.push(["something\nwith\nnewlines"]);
+    table.push(['something\nwith\nnewlines'])
 
     var expected = [
-        '┌───────────┐'
-      , '│ something │'
-      , '│ with      │'
-      , '│ newlines  │'
-      , '└───────────┘'
-    ];
+      '┌───────────┐',
+      '│ something │',
+      '│ with      │',
+      '│ newlines  │',
+      '└───────────┘'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   },
 
-  'test newlines in vertical cell header and body': function() {
-    var table = new Table({ style: {'padding-left':0, 'padding-right':0, head:[], border:[]} });
+  'test newlines in vertical cell header and body': function () {
+    var table = new Table({ style: {'padding-left': 0, 'padding-right': 0, head: [], border: []} })
 
     table.push(
         {'v\n0.1': 'Testing\nsomething cool'}
-    );
+    )
 
     var expected = [
-        '┌───┬──────────────┐'
-      , '│v  │Testing       │'
-      , '│0.1│something cool│'
-      , '└───┴──────────────┘'
-    ];
+      '┌───┬──────────────┐',
+      '│v  │Testing       │',
+      '│0.1│something cool│',
+      '└───┴──────────────┘'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   },
 
-  'test newlines in cross table header and body': function() {
-    var table = new Table({ head: ["", "Header\n1"], style: {'padding-left':0, 'padding-right':0, head:[], border:[]} });
+  'test newlines in cross table header and body': function () {
+    var table = new Table({ head: ['', 'Header\n1'], style: {'padding-left': 0, 'padding-right': 0, head: [], border: []} })
 
-    table.push({ "Header\n2": ['Testing\nsomething\ncool'] });
+    table.push({ 'Header\n2': ['Testing\nsomething\ncool'] })
 
     var expected = [
-        '┌──────┬─────────┐'
-      , '│      │Header   │'
-      , '│      │1        │'
-      , '├──────┼─────────┤'
-      , '│Header│Testing  │'
-      , '│2     │something│'
-      , '│      │cool     │'
-      , '└──────┴─────────┘'
-    ];
+      '┌──────┬─────────┐',
+      '│      │Header   │',
+      '│      │1        │',
+      '├──────┼─────────┤',
+      '│Header│Testing  │',
+      '│2     │something│',
+      '│      │cool     │',
+      '└──────┴─────────┘'
+    ]
 
-    table.toString().should.eql(expected.join("\n"));
+    table.toString().should.eql(expected.join('\n'))
   }
-};
+}
